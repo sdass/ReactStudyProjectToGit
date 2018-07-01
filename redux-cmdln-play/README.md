8c5d0a1bfa971900a0a83d0d7349bb88a9f401e9
sdass <sdass@drf.com>
2018-07-01 16:39:14 -0400

    asynchronous api call with middleware thunk.

--- redux-cmdln-play/README.md
+++ redux-cmdln-play/README.md
@@ -6,3 +6,6 @@ npm install --save redux
 2nd commit based on https://www.youtube.com/watch?v=gBER4Or86hE
 
 3rd commit based on https://www.youtube.com/watch?v=DJ8fR0mZM44 on Redux middleware
+
+4th commit based upon https://www.youtube.com/watch?v=Td-2D-_7Y2E
+for asynchronous call to rest api endpoint (https://api.publicapis.org/categories) with thunk middleware
--- redux-cmdln-play/src/index.js
+++ redux-cmdln-play/src/index.js
@@ -5,12 +5,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 //import './index.css';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk'; //thunk is a redux middleware
+import axios from "axios";//xhrRequest lib
+
+const apiURL = "https://api.publicapis.org/categories";
 
 const defaults = { name: 'subra', hobby: 'chess', age: 32 };
 //create own middleware
 const logger_myownMiddleware = (store)=>(next)=>(action)=>{
  // if(typeof action.payload === "object"){
-  console.log("action fired.." , action.type, action);
+  //console.log("action fired.." , action.type, action);
   if(action.type === 'ERROR_R'){
     next(myown_errorMiddleware);
   }else{
@@ -21,14 +26,14 @@ const logger_myownMiddleware = (store)=>(next)=>(action)=>{
 
 const myown_errorMiddleware = (store)=>(next)=>(action)=>{
   // if(typeof action.payload === "object"){
-     console.log("Entered . . . myown_errorMiddleware...");
+    // console.log("Entered . . . myown_errorMiddleware...");
      try{
       next(action);  
      } catch(e){
        console.error('CCCRRRAAAASSSSHHHH! ' + e);
      }
  }
-const myMiddleware = applyMiddleware(logger_myownMiddleware, myown_errorMiddleware); //to add any number of middleware pass as args.
+const myMiddleware = applyMiddleware(thunk, logger, logger_myownMiddleware, myown_errorMiddleware); //to add any number of middleware pass as args.
 
 const userReducer = (state=defaults, action) => {
   //console.log('userReducer()... called');
@@ -36,8 +41,8 @@ const userReducer = (state=defaults, action) => {
     case "CHANGE_NAME": {
       state = {...state, name: action.payload}
      // state.name = action.payload;
-      console.log('defaults' + JSON.stringify(defaults));
-      console.log('state' + JSON.stringify(state));
+     // console.log('defaults' + JSON.stringify(defaults));
+     // console.log('state' + JSON.stringify(state));
       break;
     }
     case "CHANGE_HOBBY": {
@@ -59,14 +64,35 @@ const todoListReducer = (state=[], actions) => { //state={}
   return state;
 };
 
+const initialState = { fetching: false, fetched: false, data:{}, error: null}
+
+const reducer2 = function(state=initialState, action){
+  //console.log('reducer called...');
+  switch(action.type){
+   case "FETCH_DATA_START":
+     state = {...state, fetching: true};
+  break;    
+  case "RECEIVED_DATA":
+  state = {...state, fetching: false, fetched: true, data: action.payload}
+  break;    
+  case "DATA_FETCH_ERROR":
+  state = {...state, fetching: false, error: action.payload};
+  break;    
+  
+  }
+  return state; //imprtant to change the state
+}
+
 const singlepointReducer = combineReducers({
   user: userReducer, //what state : what reducer to handle
   listTodo: todoListReducer,
+  ajaxapi: reducer2 // seems reducer2 got to be forward declaring
+
 });
 
 
 const reducer = function(state, action){
-  console.log('reducer called...');
+  //console.log('reducer called...');
   switch(action.type){
     case "INC":
         state = state + action.payload;
@@ -79,7 +105,6 @@ const reducer = function(state, action){
      //state = state -10;
      state = state -action.payload;
   break;    
-  
   }
   return state; //imprtant to change the state
 }
@@ -88,10 +113,29 @@ const store = createStore(singlepointReducer, {}, myMiddleware); //(singlepointR
      
 store.subscribe( () => {console.log('store changed...', store.getState())} );
 
-store.dispatch({ type: "CHANGE_NAME", payload: 'Davis Donald' });
-store.dispatch({ type: "ERROR_R", payload: -5 }); //errorHandler middlewre will catch
-store.dispatch({ type: "CHANGE_HOBBY", payload: 'hacking'});
-store.dispatch({ type: "CHANGE_USER", payload: {name: 'X45', hobby: 'U78r', age: 451} });
+store.dispatch((dispatcher2)=>{ 
+  /*
+  //console.log('Inside new way . . .1 ');
+  dispatcher2({type: 'A'}); 
+  console.log('Inside new way . . .2');
+    // do something asynch
+    dispatcher2({type: "CHANGE_NAME"}); //thunk middleware allow multiple call to dispatcher2
+    */
+   dispatcher2({type: 'FETCH_DATA_START'}); // UI will show loading spinner
+   //do asynch call
+   axios.get(apiURL)
+   //axios.get(apiURL+'/gberror') // to simiulate error
+   .then((response) =>{ 
+     dispatcher2({type: 'RECEIVED_DATA', payload: response.data})
+   }).catch((err) =>{
+     dispatcher2({type: 'DATA_FETCH_ERROR', payload: err})
+   })
+});
+
+//store.dispatch({ type: "CHANGE_NAME", payload: 'Davis Donald' });
+//store.dispatch({ type: "ERROR_R", payload: -5 }); //errorHandler middlewre will catch
+//store.dispatch({ type: "CHANGE_HOBBY", payload: 'hacking'});
+//store.dispatch({ type: "CHANGE_USER", payload: {name: 'X45', hobby: 'U78r', age: 451} });
 
 
 /*
