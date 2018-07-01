e970e2bca064b0c5aefc17952fcc9f7c2d6fc798
sdass <sdass@drf.com>
2018-07-01 14:03:42 -0400

    chaining redux middleware with next(). At the end of chain is reducer. In the middle other middleware that can be called by passing as arg inside next().

--- redux-cmdln-play/README.md
+++ redux-cmdln-play/README.md
@@ -4,3 +4,5 @@
 npm install --save redux
 
 2nd commit based on https://www.youtube.com/watch?v=gBER4Or86hE
+
+3rd commit based on https://www.youtube.com/watch?v=DJ8fR0mZM44 on Redux middleware
--- redux-cmdln-play/src/index.js
+++ redux-cmdln-play/src/index.js
@@ -3,18 +3,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import {createStore, combineReducers} from 'redux';
+//import './index.css';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 
 const defaults = { name: 'subra', hobby: 'chess', age: 32 };
+//create own middleware
+const logger_myownMiddleware = (store)=>(next)=>(action)=>{
+ // if(typeof action.payload === "object"){
+  console.log("action fired.." , action.type, action);
+  if(action.type === 'ERROR_R'){
+    next(myown_errorMiddleware);
+  }else{
+    next(action);  
+  } 
+    
+}
+
+const myown_errorMiddleware = (store)=>(next)=>(action)=>{
+  // if(typeof action.payload === "object"){
+     console.log("Entered . . . myown_errorMiddleware...");
+     try{
+      next(action);  
+     } catch(e){
+       console.error('CCCRRRAAAASSSSHHHH! ' + e);
+     }
+ }
+const myMiddleware = applyMiddleware(logger_myownMiddleware, myown_errorMiddleware); //to add any number of middleware pass as args.
+
 const userReducer = (state=defaults, action) => {
   //console.log('userReducer()... called');
   switch(action.type){
     case "CHANGE_NAME": {
       state = {...state, name: action.payload}
      // state.name = action.payload;
-      //console.log('defaults' + JSON.stringify(defaults));
-      //console.log('state' + JSON.stringify(state));
+      console.log('defaults' + JSON.stringify(defaults));
+      console.log('state' + JSON.stringify(state));
       break;
     }
     case "CHANGE_HOBBY": {
@@ -41,6 +64,7 @@ const singlepointReducer = combineReducers({
   listTodo: todoListReducer,
 });
 
+
 const reducer = function(state, action){
   console.log('reducer called...');
   switch(action.type){
@@ -60,13 +84,15 @@ const reducer = function(state, action){
   return state; //imprtant to change the state
 }
 
-const store = createStore(singlepointReducer); //,  initial state={} 
+const store = createStore(singlepointReducer, {}, myMiddleware); //(singlepointReducer); //,  initial state={} 
      
 store.subscribe( () => {console.log('store changed...', store.getState())} );
 
 store.dispatch({ type: "CHANGE_NAME", payload: 'Davis Donald' });
+store.dispatch({ type: "ERROR_R", payload: -5 }); //errorHandler middlewre will catch
 store.dispatch({ type: "CHANGE_HOBBY", payload: 'hacking'});
 store.dispatch({ type: "CHANGE_USER", payload: {name: 'X45', hobby: 'U78r', age: 451} });
+
 
 /*
 ReactDOM.render(
